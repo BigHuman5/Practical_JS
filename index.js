f5a57431b8a8c85fb1cfbe5b8f2d7cdb9657588b
Human <52690624+BigHuman5@users.noreply.github.com>
2020-07-04 19:43:26 +0600

    First

---
+++ index.js
@@ -0,0 +1,26 @@
+// подключение express
+const express = require("express");
+const bodyParser = require('body-parser');
+// создаем объект приложения
+const app = express();
+// Подключаем body-parser
+app.use(bodyParser.json());
+
+// Определяем обработчик корневого урла с методом POST
+app.post("/", function(request, response) {
+    const { firstArg, secondArg, operation } = request.body; // деструктуризация
+    if (operation === '+') {
+        response.status(200).json(firstArg + secondArg);
+    } else if (operation === '-') {
+        response.status(200).json(firstArg - secondArg);
+    } else if (operation === '/') {
+        response.status(200).json(firstArg / secondArg);
+    } else if (operation === '*') {
+        response.status(200).json(firstArg * secondArg);
+    }
+    else {
+    	response.send("<h2>Привет Express!</h2>");
+    }
+});
+// начинаем прослушивать подключения на 3000 порту
+app.listen(3000);
