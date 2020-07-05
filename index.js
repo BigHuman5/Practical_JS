9fb2a954b184bf9ff60c6bb93d340d4179418e9b
Seregazlo <52690370+Seregazlo@users.noreply.github.com>
2020-07-05 15:51:08 +0600

    Add new version

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
-// подключение express
 const express = require("express");
 const bodyParser = require('body-parser');
-// создаем объект приложения
+const { getTwoArgsOperation } = require("./operations");
+
 const app = express();
-// Подключаем body-parser
 app.use(bodyParser.json());
 
-// Определяем обработчик корневого урла с методом POST
-app.post("/", function(request, response) {
-    const { firstArg, secondArg, operation } = request.body; // деструктуризация
-    switch(operation)
-    {
-        case '+': response.status(200).json(firstArg + secondArg); break;
-        case '-': response.status(200).json(firstArg - secondArg); break;
-        case '/': { 
-            if(secondArg==0) response.status(400).json('Второй аргумент равен 0');
-            else response.status(200).json(firstArg / secondArg); 
-            break; }
-        case '*': response.status(200).json(firstArg * secondArg); break;
-        case '^': response.status(200).json(Math.pow(firstArg,secondArg)); break;
-        default: response.status(400).json('Ошибка'); break;
+app.post("/two-args", function(request, response) {
+    const { firstArg, secondArg, operation } = request.body;
+    try {
+        response
+            .status(200)
+            .json(getTwoArgsOperation(operation)(firstArg, secondArg));
+    } catch (e) {
+        response.status(400).json(e.message);
     }
 });
-// начинаем прослушивать подключения на 3000 порту
-app.listen(3000);
+
+app.listen(3000, () => {
+    console.log('App started on https://localhost:3000');
+});
